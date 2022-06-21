43f59a541cfddeb798149a92b8d961c13de0a0d1
Bolanihun <105159054+Bolanihun@users.noreply.github.com>
2022-06-21 15:29:06 +0100

    Add files via upload

---
+++ INFO/index.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+const nAme = document.querySelector("h1")
+const height = document.querySelector("h2")
+const country = document.querySelector("h3")
+
+nAme.innerHTML = "AISHAH"
+height.textContent = "3.5m"
+country.innerText = "NIGERIA"
+
+
